20d14ced8d2bb8d32ad143c56f63a72ad3a975b2
waire <waire.tega@gmail.com>
2021-12-26 09:22:33 +0100

    added service interface and begins routing

--- internal/handlers/carts/http.go
+++ internal/handlers/carts/http.go
@@ -1,6 +1,12 @@
 package handlers
 
-import "cart/internal/core/ports/cartPort"
+import (
+	"cart/internal/core/domain"
+	"cart/internal/core/ports/cartPort"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 type HTTPHandler struct {
 	cartService cartPort.CartService
@@ -12,8 +18,25 @@ func NewHTTPHandler(cartService cartPort.CartService) *HTTPHandler {
 	}
 }
 
-func (handler *HTTPHandler) Get() {
+func (handler *HTTPHandler) Get(w http.ResponseWriter, r *http.Request) {
+	cart := domain.Cart{}
+	newCart, err := handler.cartService.Get(cart.Id)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	json.NewDecoder(r.Body).Decode(&newCart)
 	// Get(id string) (domain.Cart, error)
 	// Save(domain.Cart) (domain.Cart, error)
 
+}
+
+func (handler *HTTPHandler) Save(w http.ResponseWriter, r *http.Request) {
+	cart := domain.Cart{}
+	newCart, err := handler.cartService.Save(domain.NewCart(cart.Name, cart.State))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	json.NewEncoder(w).Encode(newCart)
 }
